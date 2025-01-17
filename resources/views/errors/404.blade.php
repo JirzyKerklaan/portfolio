1eb4602e52fcaa48fe3d1b2a26dbe611d1231e2c
JirzyKerklaan <jirzykerklaan@gmail.com>
2025-01-17 18:50:02 +0100

    added custom 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" style=" overflow: hidden;">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 | Page not found</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body style="background-color: #0f0f0f; color: #fff; font-family: Inter, sans-serif; max-height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center; overflow: hidden;">
+    <div class="404" style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
+        <h1 style="font-size: 4rem; margin-bottom: .5rem;"><span style="color: #1BDDB1;">4</span>0<span style="color: #1BDDB1;">4</span></h1>
+        <p style="font-size: calc(22rem / 16);">Pagina niet gevonden</p>
+        <a href="/">Terug naar homepagina</a>
+    </div>
+    <style>
+        * {
+            font-family: 'Inter', sans-serif;
+        }
+
+        a {
+            border: 2px solid #0F0F0F;
+            padding: 1rem 2rem;
+            background: #FAFAFA;
+            border-radius: 999px;
+            color: #0F0F0F;
+            text-decoration: none;
+            font-size: 1.25rem;
+            transition: all ease-in-out .15s;
+        }
+        a:hover {
+            border: 2px solid #FAFAFA !important;
+            background: #0F0F0F !important;
+            color: #FAFAFA !important;
+            transition: all ease-in-out .15s;
+        }
+    </style>
+</body>
+</html>
