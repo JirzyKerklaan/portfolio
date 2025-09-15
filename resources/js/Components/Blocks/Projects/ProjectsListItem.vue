9f99566bcf064f4d9cec21e9bc05f9261287d963
JirzyKerklaan <jirzykerklaan@gmail.com>
2025-09-15 20:45:56 +0200

    removed line animation projects

--- resources/js/Components/Blocks/Projects/ProjectsListItem.vue
+++ resources/js/Components/Blocks/Projects/ProjectsListItem.vue
@@ -1,40 +1,10 @@
 <script setup>
-import gsap from 'gsap'
-import ScrollTrigger from 'gsap/ScrollTrigger'
-import {onMounted} from "vue";
-
 defineProps({
     name: String,
     task: String,
     i: Number,
     url: String
 });
-
-gsap.registerPlugin(ScrollTrigger)
-
-const animateProjectUnderlines = () => {
-    const elements = document.querySelectorAll('.projects__list--item .underline')
-
-    elements.forEach((el, index) => {
-        gsap.to(el, {
-            scaleX: 1,
-            ease: 'power2.out',
-            transformOrigin: 'left',
-            scrollTrigger: {
-                trigger: el.parentElement,
-                start: `top+=${index * 100} 70%`,
-                end: `top+=${index * 100 + 200} 95%`,
-                scrub: true,
-            }
-        })
-    })
-}
-
-
-onMounted(() => {
-   animateProjectUnderlines()
-});
-
 </script>
 
 <template>
